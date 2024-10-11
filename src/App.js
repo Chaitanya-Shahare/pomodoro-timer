170954cba681b13e632cdf6a1d929a6870942b88
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2024-10-11 11:36:10 +0530

    Complete basic pomodoro timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    if (isRunning) {
+      let timer = setInterval(() => {
+        if (seconds === 0) {
+
+          if (minutes === 0) {
+            setIsRunning(false);
+            return;
+          }
+
+          setSeconds(59);
+          setMinutes((min) => min - 1);
+        } else {
+          setSeconds((sec) => sec - 1);
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [isRunning, minutes, seconds]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setMinutes(25);
+    setSeconds(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="card__timer">
+          {minutes < 10 ? "0" + minutes : minutes}:
+          {seconds < 10 ? "0" + seconds : seconds}
+        </div>
+        <div className="card__actions">
+          <button className="card__btn card__btn--green" onClick={handleStart}>
+            Start
+          </button>
+          <button className="card__btn card__btn--grey" onClick={handlePause}>
+            Pause
+          </button>
+          <button className="card__btn card__btn--red" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
